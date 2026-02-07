60d5c635399c3781b251407f6c0cccc29195ae94
Lamees <lamees1harbi@gmail.com>
2026-02-07 12:16:18 +0300

    added folders and pages about hompage

---
+++ HomePage/View/HomeScreen.swift
@@ -0,0 +1,19 @@
+//
+//  HomeScreen.swift
+//  Haik
+//
+//  Created by lamess on 07/02/2026.
+//
+
+import SwiftUI
+import MapKit
+
+struct HomeScreen: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
