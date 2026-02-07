3c7a230bc3170fa85c0f4b20bfc79522eab5794e
Maryam <maryamjalzh@gmail.com>
2026-02-07 13:12:41 +0300

    Added Favorite page, added color set

---
+++ FavoritePage/View/FavoritePage.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritePage.swift
+//  Haik
+//
+//  Created by Maryam Jalal Alzahrani on 19/08/1447 AH.
+//
+
+import SwiftUI
+
+struct FavoritePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritePage()
+}
